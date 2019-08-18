a52fc5dd2745acffbea58e112091897a44a9727c
Andrius Jurgsaitis <andriusj6@hotmail.com>
2019-08-18 18:53:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,8 +1,9 @@
-def create_an_empty_array []
+def create_an_empty_array
+  []
 end
 
 def create_an_array
-  
+  ["Bacon", "Eggs", "Beans", "Toast"]
 end
 
 def add_element_to_end_of_array(array, element)
